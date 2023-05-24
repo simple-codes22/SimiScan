e546ca33b1a46318710320ff44883eac6b12eb02
Simeon Akanbi <simeonakanbi99@gmail.com>
2023-05-24 12:53:55 +0100

    Fixed a copy issue with the ToggleCopy hook

--- src/misc/copy.jsx
+++ src/misc/copy.jsx
@@ -44,7 +44,7 @@ export const ToggleCopy = ({ number }) => {
       () => {
         const copyBtn = document.querySelector(`.${number}`)
         copyBtn.innerHTML = Copied;
-        const text = copyBtn.parentNode.firstChild.textContent;
+        const text = copyBtn.parentNode.firstChild.getAttribute('title');
         navigator.clipboard.writeText(text)
         setTimeout(() => {
           copyBtn.innerHTML = contentCopy;
